551507aca80ac87404a24439f0a048e9f197a322
Chandrakant Mali <161549812+chandrakant-mali85@users.noreply.github.com>
2025-05-23 17:27:49 +0530

    done

---
+++ ArrayListExample.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+
+public class ArrayListExample{
+
+	public static void main(String args[]){
+
+		ArrayList <String>fruits=new ArrayList<String>();
+
+		fruits.add("Apple");
+		fruits.add("Banana");
+
+
+
+		fruits.set(0,"chiku");
+		System.out.println(fruits);
+
+			System.out.println(fruits.size());
+
+
+
+
+
+
+
+
+	}
+
+}
---
+++ Demo.java
@@ -0,0 +1,25 @@
+// ArrayList Example
+
+import java.util.ArrayList;
+
+public class Main{
+
+	public static void main(String args[]){
+
+		ArrayList<String>list=new ArrayList<String>();
+
+		fruits.add("Apple");
+		fruits.add("Banana");
+		fruits.add("Mango");
+		fruits.add("Orange");
+
+
+		System.out.println("Fruits:"+fruits);
+
+		System.out.println("Size:"+fruits.size());
+		fruits.remove("Banana");
+		System.out.println("After removing Banana:"+fruits);
+		System.out.println("Index of Mango:"+fruits.indexOf("Mongo");
+		System.out.println("Contains Apple:"+fruits.contains("Apple"));
+	}
+}
